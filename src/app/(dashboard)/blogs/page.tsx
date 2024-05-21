f08c1861e427e07971c59f28fd83f87f2fdef6ed
Freddy Tomada <freddytomada7@gmail.com>
2024-05-21 09:47:48 -0400

    add button to create blog in blogs page

--- src/app/(dashboard)/blogs/page.tsx
+++ src/app/(dashboard)/blogs/page.tsx
@@ -1,19 +1,34 @@
 import { loadBlogs } from "@/lib/utils";
 import BlogCard, { BlogCardProps } from "./_components/blogCard";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { PlusSquare } from "lucide-react";
 
 export default async function BlogsPage() {
   const blogs: BlogCardProps[] = await loadBlogs();
 
   return (
     <div className="px-12 h-full">
-      <h1 className="text-4xl my-8 font-bold w-full text-center">Blogs</h1>
+      <div className="flex items-center">
+        <h1 className="text-4xl my-8 font-bold w-full text-center">Blogs</h1>
+        <Link href="/blogs/new">
+          <Button variant={"main"}>
+            <PlusSquare size={24} className="mr-2" />
+            Create Blog
+          </Button>
+        </Link>
+      </div>
       <div className="grid grid-cols-4 gap-8">
-        <div className="border-2 border-black">
-          {/* Filters and Search */}
-        </div>
+        <div className="border-2 border-black">{/* Filters and Search */}</div>
         <div className="flex flex-col gap-8 col-span-3">
           {blogs.map((blog) => (
-            <BlogCard key={blog.id} id={blog.id} title={blog.title} description={blog.description} image={blog.image} />
+            <BlogCard
+              key={blog.id}
+              id={blog.id}
+              title={blog.title}
+              description={blog.description}
+              image={blog.image}
+            />
           ))}
         </div>
       </div>
