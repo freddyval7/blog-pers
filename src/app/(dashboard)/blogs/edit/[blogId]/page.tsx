58a712174cca48afe0bf67c39b90005d9311ebb2
Freddy Tomada <freddytomada7@gmail.com>
2024-05-21 09:47:29 -0400

    refactor mt in forms blog

--- src/app/(dashboard)/blogs/edit/[blogId]/page.tsx
+++ src/app/(dashboard)/blogs/edit/[blogId]/page.tsx
@@ -10,7 +10,7 @@ export default async function EditBlogPage({
   const blog: BlogCardProps = await loadBlog(params.blogId);
 
   return (
-    <div className="w-1/2 md:w-1/3 border-2 rounded-md shadow-md p-8 mx-auto mt-40 space-y-8">
+    <div className="w-1/2 md:w-1/3 border-2 rounded-md shadow-md p-8 mx-auto mt-20 space-y-8">
       <h1 className="text-center text-2xl font-bold">
         Edit Blog {`"${blog.title}"`}
       </h1>
--- src/app/(dashboard)/blogs/new/page.tsx
+++ src/app/(dashboard)/blogs/new/page.tsx
@@ -2,7 +2,7 @@ import BlogForm from "./_components/blogForm";
 
 export default function NewBlogPage() {
   return (
-    <div className="w-1/2 md:w-1/3 border-2 rounded-md shadow-md p-8 mx-auto mt-40 space-y-8">
+    <div className="w-1/2 md:w-1/3 border-2 rounded-md shadow-md p-8 mx-auto mt-20 space-y-8">
       <h1 className="text-center text-2xl font-bold">Create New Blog</h1>
       <BlogForm />
     </div>
