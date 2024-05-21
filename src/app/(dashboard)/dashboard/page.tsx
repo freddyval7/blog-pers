dd517a7a6517d1d9d60c986ca554f0ff0ec464a7
Freddy Tomada <freddytomada7@gmail.com>
2024-05-21 09:30:10 -0400

    add provitional dashboard page

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,7 @@
 export default function DashboardPage() {
   return (
-    <div>Dashboard</div>
+    <div className="mt-52">
+      <h1 className="text-center font-bold text-muted-foreground text-3xl italic">Coming Soon!...</h1>
+    </div>
   )
 }
