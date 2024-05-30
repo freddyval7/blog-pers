953b58625f26b258800a43cefb3d35573729f21c
Freddy Tomada <freddytomada7@gmail.com>
2024-05-30 16:11:40 -0400

    add author when create blog

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -1,15 +1,34 @@
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth/next";
 
 export async function POST(req: Request, res: Response) {
   const data = await req.json();
+  const session = await getServerSession();
+  if (!session) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+  const user = await prisma.user.findUnique({
+    where: {
+      email: session.user && session.user.email as string,
+    },
+  });
+
+  if (!user) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
 
   try {
     await prisma.blog.create({
       data: {
         title: data.title,
         content: data.content,
+        author: {
+          connect: {
+            id: user.id,
+          },
+        }
       },
     });
     revalidatePath("/blogs");
